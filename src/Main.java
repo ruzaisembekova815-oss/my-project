903d5d6d2a7957a259e38978bd9fcfd9afa5b35e
Ruza Isembekova <253458@astanait.edu.kz>
2025-12-18 15:28:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Rectangles r = new Rectangles(3, 4);
+        System.out.println(r.toString());
+    }
+}
+
---
+++ src/Rectangles.java
@@ -0,0 +1,64 @@
+import java.util.Set;
+
+public class Rectangles {
+    private double width;
+    private double height;
+    private int id;
+    private static int idGen;
+
+    public Rectangles (){
+        this.width = 1.0;
+        this.height = 1.0;
+        this.id = idGen++;
+
+    }
+
+    public Rectangles(double width, double height) {
+        this();
+        setWidth (width);
+        setHeight (height);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setWidth(double width) {
+        if (width <= 0) {
+            throw new IllegalArgumentException("Width must be greater than 0");
+        }
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        if (height < 0) {
+            throw new IllegalArgumentException("Width must be greater than 0");
+        }
+        this.height = height;
+    }
+    public double area() {
+        return width * height;}
+
+    public double perimeter(){
+        return 2*(width + height);
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Rectangles{id=" + id +
+                ",height=" + height +
+                ", width=" + width +"}";
+    }
+}
+
+
