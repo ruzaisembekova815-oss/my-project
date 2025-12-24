cd51097de07e05a46d8ea067c0bee230e3a080ea
ruzaisembekova815-oss <253458@astanait.edu.kz>
2025-12-24 03:20:18 +0300

    Update Rectangles.java

--- src/Rectangles.java
+++ src/Rectangles.java
@@ -4,9 +4,7 @@ public class Rectangles {
     private double width;
     private double height;
     private int id;
-    private static int idGen;
-
-    public 
+    private static int idGen; 
 
     public Rectangles (){
         this.width = 1.0;
